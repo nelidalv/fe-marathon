5437df67bf8cf865717b05607d7aacdc1e3463ed
nelidalv <alt0151@gmail.com>
2022-07-15 19:44:34 +0300

    04

--- 04-board/app.js
+++ 04-board/app.js
@@ -1,6 +1,6 @@
 const board = document.querySelector('#board')
-const colors = ['#e74c3c', '#8e44ad', '#349db', '#e67e22', '#2ecc71']
-const SQUAERS_NUMBER = 100
+// const colors = ['#e74c3c', '#8e44ad', '#349db', '#e67e22', '#2ecc71']
+const SQUAERS_NUMBER = 300
 
 for (let i = 0 ; i < SQUAERS_NUMBER; i++) {
     const square = document.createElement('div') // в строковом виде говорим какой тег хотим создать
@@ -14,7 +14,11 @@ for (let i = 0 ; i < SQUAERS_NUMBER; i++) {
 }
 
 function setColor(element) {
-    const color = getRandomColor()
+    // const color = getRandomColor()
+    const color = '#'+ getRandomIntInclusive().toString(16)
+    // const color = '#'+ (0x100000 + Math.floor(Math.random() * 0xffffff)).toString(16)
+
+    console.log(color)
     element.style.backgroundColor = color
     element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
@@ -23,7 +27,13 @@ function removeColor(element) {
     element.style.boxShadow = `0 0 2px #000`
 }
 
-function getRandomColor() {
-    const index = Math.floor(Math.random() * colors.length)
-    return colors[index]
+// function getRandomColor() {
+//     const index = Math.floor(Math.random() * colors.length)
+//     return colors[index]
+// }
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(0x100000);
+    max = Math.floor(0xffffff);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
